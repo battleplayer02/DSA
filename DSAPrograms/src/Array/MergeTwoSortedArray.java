695421a1ffc00fcfd243a370eff146b4e6fd3a27
Himanshu Shekhar <42701850+battleplayer02@users.noreply.github.com>
2021-07-14 12:15:09 +0530

    merge two sorted array

--- DSAPrograms/src/Array/MergeTwoSortedArray.java
+++ DSAPrograms/src/Array/MergeTwoSortedArray.java
@@ -5,6 +5,8 @@
  */
 package Array;
 
+import java.util.*;
+
 /**
  *
  * @author Administrator
@@ -12,5 +14,43 @@ package Array;
 public class MergeTwoSortedArray {
 
     public static void main(String[] args) {
+        int[] arr1 = { 1, 2, 3, 4, 5 };
+        int[] arr2 = { 6, 7, 8, 9, 10 };
+        int arr[] = merge(arr1, arr2);
+        // create arrayList from array
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < arr.length; i++) {
+            list.add(arr[i]);
+        }
+        System.out.println(list);
+    }
+
+    // function to merge two sorted arrays
+    public static int[] merge(int[] a, int[] b) {
+        int arr[] = new int[a.length + b.length];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while (i < a.length && j < b.length) {
+            if (a[i] < b[j]) {
+                arr[k] = a[i];
+                i++;
+            } else {
+                arr[k] = b[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < a.length) {
+            arr[k] = a[i];
+            i++;
+            k++;
+        }
+        while (j < b.length) {
+            arr[k] = b[j];
+            j++;
+            k++;
+        }
+        return arr;
     }
 }
