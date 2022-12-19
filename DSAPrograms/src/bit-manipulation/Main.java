1026d017f6337e5acf933eca535f170f83513262
Himanshu Shekhar <42701850+battleplayer02@users.noreply.github.com>
2022-12-19 16:30:54 +0000

    bit

--- DSAPrograms/src/bit-manipulation/Main.java
+++ DSAPrograms/src/bit-manipulation/Main.java
@@ -1,10 +1,31 @@
 import java.util.*;
 
-public class Main {
+public class Main { 
     // main
     public static void main(String[] args) {
-        System.out.println(bin(2));
+        ans(0, 1, 5, 6, 3);        
     }
+
+    public static void ans(int num, int i, int j, int k, int m) {
+        /**
+         * Turn ON  ith bit
+         * Turn OFF jth bit
+         * Toggle   kth bit
+         * Check    mth bit
+         **/
+         turnON(num, i);
+    }
+
+    public static void turnON(int n, int i){
+        // 10000000000
+        // 00000010000
+        // 10000010000
+        int msk = 1<<i;
+        System.out.println(bin(msk));
+        int res = n | msk;
+        System.out.println(i+"th bit turned on of "+bin(n)+"="+bin(res));
+    }
+
     public static String bin(int num){
         List<Integer> st = new ArrayList<Integer>();
         while(num!=0){
