c102fc8d6c1fec76ff1ae0d2b33abf9c68d2a844
Himanshu Shekhar <42701850+battleplayer02@users.noreply.github.com>
2022-12-19 10:05:18 +0000

    bit

--- DSAPrograms/src/bit-manipulation/Main.java
+++ DSAPrograms/src/bit-manipulation/Main.java
@@ -1,7 +1,18 @@
+import java.util.*;
+
 public class Main {
     // main
     public static void main(String[] args) {
-    
+        System.out.println(bin(2));
     }
+    public static String bin(int num){
+        List<Integer> st = new ArrayList<Integer>();
+        while(num!=0){
+            st.add(num%2);
+            num/=2;
+        }
 
+        Collections.reverse(st);
+        return st.toString();
+    }
 }
