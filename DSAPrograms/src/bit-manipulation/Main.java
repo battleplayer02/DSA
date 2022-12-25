cb9dcc80ac7519117c87ce3efdbab2d4357f461c
Himanshu Shekhar <42701850+battleplayer02@users.noreply.github.com>
2022-12-25 05:38:14 +0000

    bit manupulation

--- DSAPrograms/src/bit-manipulation/Main.java
+++ DSAPrograms/src/bit-manipulation/Main.java
@@ -1,9 +1,27 @@
 import java.util.*;
 
+import javax.security.cert.X509Certificate;
+
 public class Main { 
     // main
     public static void main(String[] args) {
-        ans(0, 1, 5, 6, 3);        
+        // ans(0, 1, 5, 6, 3);
+        // int num = 16;
+        // StringBuilder bin = new StringBuilder("");
+        // while(num!=0){
+        //     int rem = num%2;
+        //     bin.append(rem);
+        //     num/=2;
+        // }
+        // System.out.println(bin.reverse());
+        // 1010111
+        // 1001000
+        // 1000000
+        int x = 42;
+        int msk = ~(1<<3);
+        System.out.println(Integer.toBinaryString(x));
+        System.out.println(Integer.toBinaryString(msk).substring(26));
+        System.out.println(Integer.toBinaryString(x&msk));
     }
 
     public static void ans(int num, int i, int j, int k, int m) {
@@ -38,4 +56,14 @@ public class Main {
         Collections.reverse(st);
         return st.toString();
     }
+
+
+
+
+
+
+
+
+
+
 }
